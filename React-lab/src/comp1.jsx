da8af959e380a03175f58a1a89ce80644ac139f8
Krishna Kumar <krishnakumarmahtoce@gmail.com>
2024-12-21 14:35:50 +0530

    First change in src

---
+++ React-lab/src/comp1.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return ( 
+        <div>
+            Hello World
+        <h1>Hello, world!</h1>
+        <h2>This H2 tag</h2>
+        <h3>H3 Tag</h3>
+        <p>This is a paragraph.</p>
+        <input type="text" name="d" id="a" />
+        <br />
+        <br />
+        <input type="text" name="Name" id="" />
+        <br />
+        <input type="button" value="SUBMIT" />
+           </div>
+        );
+    }
+}
+export default MyComponent;
